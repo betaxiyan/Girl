1673494b39f9511481c97c061d313f2749f1bb08
wangbin <wangbin@bonc.com.cn>
2017-10-25 22:25:57 +0800

    可打War包

--- src/main/java/com/imooc/GirlApplication.java
+++ src/main/java/com/imooc/GirlApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GirlApplication extends SpringBootServletInitializer  {
+public class GirlApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GirlApplication.class, args);
 	}
-	
-	@Override
+
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // TODO Auto-generated method stub
         return builder.sources(GirlApplication.class);
